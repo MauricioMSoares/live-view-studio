c78ef131810e5001bb29a5bb9653c721fcf5cdbf
MauricioMSoares <mauricio.soares@sptech.school>
2023-11-03 14:06:20 -0300

    added content from chapter 14

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -2,14 +2,18 @@ defmodule LiveViewStudioWeb.ServersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Servers
+  alias LiveViewStudio.Servers.Server
 
   def mount(_params, _session, socket) do
     servers = Servers.list_servers()
 
+    changeset = Servers.change_server(%Server{})
+
     socket =
       assign(socket,
         servers: servers,
-        coffees: 0
+        coffees: 0,
+        form: to_form(changeset)
       )
 
     {:ok, socket}
@@ -26,10 +30,21 @@ defmodule LiveViewStudioWeb.ServersLive do
   end
 
   def handle_params(_, _uri, socket) do
-    {:noreply,
-     assign(socket,
-       selected_server: hd(socket.assigns.servers)
-     )}
+    socket =
+      if socket.assigns.live_action == :new do
+        changeset = Servers.change_server(%Server{})
+
+        assign(socket,
+          selected_server: nil,
+          form: to_form(changeset)
+        )
+      else
+        assign(socket,
+          selected_server: hd(socket.assigns.servers)
+        )
+      end
+
+    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -38,6 +53,9 @@ defmodule LiveViewStudioWeb.ServersLive do
     <div id="servers">
       <div class="sidebar">
         <div class="nav">
+          <.link patch={~p"/servers/new"} class="add">
+            + Add New Server
+          </.link>
           <.link
             :for={server <- @servers}
             patch={~p"/servers/#{server}"}
@@ -56,7 +74,40 @@ defmodule LiveViewStudioWeb.ServersLive do
       </div>
       <div class="main">
         <div class="wrapper">
-          <.server server={@selected_server} />
+          <%= if @live_action == :new do %>
+            <.form for={@form} phx-submit="save">
+              <div class="field">
+                <.input
+                  field={@form[:name]}
+                  placeholder="Name"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="field">
+                <.input
+                  field={@form[:framework]}
+                  placeholder="Framework"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="field">
+                <.input
+                  field={@form[:size]}
+                  type="number"
+                  placeholder="Size (MB)"
+                  autocomplete="off"
+                />
+              </div>
+              <.button phx-disable-with="Saving...">
+                Save
+              </.button>
+              <.link patch={~p"/servers"} class="cancel">
+                Cancel
+              </.link>
+            </.form>
+          <% else %>
+            <.server server={@selected_server} />
+          <% end %>
           <div class="links">
             <.link navigate={~p"/light"}>
               Adjust Lights
@@ -100,5 +151,30 @@ defmodule LiveViewStudioWeb.ServersLive do
 
   def handle_event("drink", _, socket) do
     {:noreply, update(socket, :coffees, &(&1 + 1))}
+  end
+
+  def handle_event("save", %{"server" => server_params}, socket) do
+    case Servers.create_server(server_params) do
+      {:ok, server} ->
+        socket =
+          update(
+            socket,
+            :servers,
+            fn servers -> [server | servers] end
+          )
+
+        changeset = Servers.change_server(%Server{})
+        socket = put_flash(socket, :info, "Server successfully created!")
+        socket = push_patch(socket, to: ~p"/servers?#{server}")
+        {:noreply, assign_form(socket, changeset)}
+
+      {:error, changeset} ->
+        socket = put_flash(socket, :error, "Something went wrong.")
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
   end
 end
--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -2,13 +2,17 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Volunteers
+  alias LiveViewStudio.Volunteers.Volunteer
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
       assign(socket,
-        volunteers: volunteers
+        volunteers: volunteers,
+        form: to_form(changeset)
       )
 
     {:ok, socket}
@@ -18,6 +22,22 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
+      <.form for={@form} phx-submit="save">
+        <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
+        <.input
+          field={@form[:phone]}
+          type="tel"
+          placeholder="Phone"
+          autocomplete="off"
+        />
+        <.button phx-disable-with="Saving...">
+          Check In
+        </.button>
+      </.form>
+      <pre>
+        <%#= inspect(@form, pretty: true) %>
+      </pre>
+
       <div
         :for={volunteer <- @volunteers}
         class={"volunteer #{if volunteer.checked_out, do: "out"}"}
@@ -36,5 +56,29 @@ defmodule LiveViewStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        socket =
+          update(
+            socket,
+            :volunteers,
+            fn volunteers -> [volunteer | volunteers] end
+          )
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+        socket = put_flash(socket, :info, "Volunteer successfully checked in!")
+        {:noreply, assign_form(socket, changeset)}
+
+      {:error, changeset} ->
+        socket = put_flash(socket, :error, "Something went wrong.")
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
   end
 end
