235bb43dd1293ef5d163f6335901cc542a85060f
MauricioMSoares <mauricio.soares@sptech.school>
2023-11-07 14:05:45 -0300

    implemented Presence channel to visualize real time users in presence and bingo, using content from chapter 22

--- lib/live_view_studio_web/live/bingo_live.ex
+++ lib/live_view_studio_web/live/bingo_live.ex
@@ -1,29 +1,62 @@
 defmodule LiveViewStudioWeb.BingoLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.Presence
+
+  @topic "users:bingo"
+
   def mount(_params, _session, socket) do
+    %{current_user: current_user} = socket.assigns
+
     if connected?(socket) do
+      Phoenix.PubSub.subscribe(LiveViewStudio.PubSub, @topic)
+
+      {:ok, _} =
+        Presence.track(self(), @topic, current_user.id, %{
+          username: current_user.email |> String.split("@") |> hd(),
+          timestamp: Timex.now() |> Timex.format!("%H:%M", :strftime)
+        })
+
       :timer.send_interval(3000, self(), :tick)
     end
 
+    presences = Presence.list(@topic)
+
     socket =
-      assign(socket,
-        number: nil,
-        numbers: all_numbers()
-      )
+      socket
+      |> assign(number: nil)
+      |> assign(numbers: all_numbers())
+      |> assign(:presences, simple_presence_map(presences))
 
     {:ok, socket}
+  end
+
+  def simple_presence_map(presences) do
+    Enum.into(presences, %{}, fn {user_id, %{metas: [meta | _]}} ->
+      {user_id, meta}
+    end)
   end
 
   def render(assigns) do
     ~H"""
     <h1>Bingo Boss 📢</h1>
     <div id="bingo">
+      <div class="users">
+        <ul>
+          <li :for={{_user_id, meta} <- @presences}>
+            <span class="username">
+              <%= meta.username %>
+            </span>
+            <span class="timestamp">
+              <%= meta.timestamp %>
+            </span>
+          </li>
+        </ul>
+      </div>
       <div class="number">
         <%= @number %>
       </div>
     </div>
-    <div><%= @current_user.id %></div>
     """
   end
 
@@ -54,5 +87,27 @@ defmodule LiveViewStudioWeb.BingoLive do
 
   def handle_info(:tick, socket) do
     {:noreply, pick(socket)}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
+    socket =
+      socket
+      |> remove_presences(diff.leaves)
+      |> add_presences(diff.joins)
+
+    {:noreply, socket}
+  end
+
+  defp remove_presences(socket, leaves) do
+    user_ids = Enum.map(leaves, fn {user_id, _} -> user_id end)
+
+    presences = Map.drop(socket.assigns.presences, user_ids)
+
+    assign(socket, :presences, presences)
+  end
+
+  defp add_presences(socket, joins) do
+    presences = Map.merge(socket.assigns.presences, simple_presence_map(joins))
+    assign(socket, :presences, presences)
   end
 end
